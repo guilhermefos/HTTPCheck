c93f840121f0235fb9ba66c3354b56a7fdf98474
guilhermefos <guilhermefos.developer@gmail.com>
2016-09-26 20:09:48 -0300

    First Commit
    change users permissions and checking network status

---
+++ app/src/main/java/com/example/httpcheck/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.httpcheck;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()){
+            // make connection
+        } else {
+            // error
+        }
+    }
+}
